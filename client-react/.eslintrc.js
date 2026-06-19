bfbef0d572b4305914c61d8ff354400fb22186d8
jiumu <1961786978@qq.com>
2026-06-19 11:37:10 +0800

    chore: Husky / ESLint / Prettier 工程化基线
    Split from f998e1e: feat: 发现页搜索筛选与分页、Aceternity 按钮库、构建与样式修复

---
+++ client-react/.eslintrc.js
@@ -0,0 +1,34 @@
+export default {
+  root: true,
+  env: {
+    browser: true,
+    es2022: true,
+    node: true,
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  plugins: ['@typescript-eslint', 'react', 'react-hooks', 'prettier'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
+  ],
+  ignorePatterns: ['dist', 'node_modules'],
+  rules: {
+    'prettier/prettier': 'error',
+    'react/react-in-jsx-scope': 'off',
+  },
+}
